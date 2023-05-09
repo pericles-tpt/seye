15fbdf1453dbc893aa6ace1ae97c66f9d9fe1266
pericles@outlook.com.au <pericles@outlook.com.au>
2023-05-10 00:09:45 +1000

    Update `main()` with a benchmark, improve `Walk()`

--- main.go
+++ main.go
@@ -1,21 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/Fiye/tree"
+)
+
 func main() {
-	s1 := FileTree{
-		BasePath: "/home/",
+	p := "/"
+	fmt.Printf("# Using path: %s\n", p)
+	for _, sleepFor := range []int{0, 1, 3, 9, 27, 81, 243} {
+		fmt.Printf("# Benchmark: %d minute(s) delay between tests\n", sleepFor)
+
+		s1 := &tree.FileTree{
+			BasePath: p,
+		}
+
+		a := time.Now()
+		s1.Walk(0)
+		fmt.Printf("  * Walk 1 took: %dms\n", time.Since(a).Milliseconds())
+
+		time.Sleep(time.Minute * time.Duration(sleepFor))
+
+		s2 := &tree.FileTree{
+			BasePath: p,
+		}
+
+		a = time.Now()
+		s2.Walk(0)
+		fmt.Printf("  * Walk 2 took: %dms\n", time.Since(a).Milliseconds())
+
+		// dt := tree.FileTree{}
+		a = time.Now()
+		diff := tree.CompareTrees(s1, s2)
+		fmt.Printf("  * Compare trees took: %dms\n\n", time.Since(a).Milliseconds())
+
+		s1 = nil
+		s2 = nil
+
+		fmt.Printf("  * Trees Diff (b - a) Summary:\n 	NewerPath: %s\n 	Number of Different Files: %d\n 	Number of Different Subtrees (non-recursive): %d\n 	Difference in 'Last Modified' Time: %dms\n 	Difference in Size: %d bytes\n	Difference in time taken: %dms\n\n", diff.NewerPath, len(diff.FilesDiff), len(diff.SubTreesDiff), diff.LastModifiedDiff.Milliseconds(), diff.SizeDiff, diff.TimeTakenDiff.Milliseconds())
+		// tree.PrintTree(&dt)
+
+		// time.Sleep(time.Second * 30)
+
+		// s2 := FileTree{
+		// 	BasePath: "/home/",
+		// }
+
+		// Walk(&s2)
+
+		// PrintTree(&s1)
+
+		// CompareTrees(&s1, &s2, nil)
 	}
-
-	Walk(&s1)
-
-	// time.Sleep(time.Second * 30)
-
-	// s2 := FileTree{
-	// 	BasePath: "/home/",
-	// }
-
-	// Walk(&s2)
-
-	// PrintTree(&s1)
-
-	// CompareTrees(&s1, &s2, nil)
 }
--- tree/walk.go
+++ tree/walk.go
@@ -3,30 +3,68 @@ package tree
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
-func Walk(tree *FileTree) {
+var (
+	Here         = time.Local
+	UnixYear2048 = time.Date(2025, 1, 1, 0, 0, 0, 0, Here).Unix()
+	AllTrees     = []FileTree{}
+)
+
+func (tree *FileTree) Walk(depth int) (size int64, latestModified *time.Time) {
+
 	ents, err := os.ReadDir(tree.BasePath)
 	tree.Err = err
 
+	var (
+		newestModified *time.Time = nil
+		startWalk                 = time.Now()
+	)
 	for _, e := range ents {
 		if e.IsDir() {
+			if strings.HasPrefix(e.Name(), ".") {
+				continue
+			}
 			subTree := FileTree{
 				BasePath: fmt.Sprintf("%s/%s", tree.BasePath, e.Name()),
 			}
-			Walk(&subTree)
+			subSize, currModified := subTree.Walk(depth + 1)
+			tree.Size += subSize
 			tree.SubTrees = append(tree.SubTrees, subTree)
-		} else {
+
+			newestModified = GetNewestTime(newestModified, currModified)
+		} else if e.Type().IsRegular() {
 			f, err := e.Info()
 			if err != nil {
 				tree.Files = append(tree.Files, File{"", 0, err})
 			} else {
 				tree.Files = append(tree.Files, File{f.Name(), f.Size(), err})
-				tree.Size += int(f.Size())
-			}
+				tree.Size += f.Size()
 
+				currModified := f.ModTime()
+				newestModified = GetNewestTime(newestModified, &currModified)
+			}
 		}
 	}
+	tree.TimeTaken = time.Since(startWalk)
+
+	tree.LastModified = newestModified
+	tree.LastVisited = time.Now()
+	tree.Depth = depth
+	tree.Priority = CalculatePriority(*tree)
+
+	AllTrees = append(AllTrees, *tree)
+
+	return tree.Size, newestModified
+}
+
+func GetNewestTime(curr *time.Time, new *time.Time) *time.Time {
+	if curr == nil || (new != nil && curr.Before(*new)) {
+		return new
+	}
+	return curr
 }
 
 // TODO: Delete this?
@@ -43,4 +81,30 @@ func PrintTree(t *FileTree) {
 	for _, t := range t.SubTrees {
 		PrintTree(&t)
 	}
+}
+
+func CalculatePriority(t FileTree) int64 {
+	// FilesDiff?
+	// DirsDiff?
+
+	// older -> good
+	// lastVisited := 10 * float64((UnixYear2048-t.LastVisited.Unix())/UnixYear2048)
+	// // newer -> good
+	// var lastModified float64 = 0
+	// if t.LastModified != nil {
+	// 	lastModified = 0 //100 * float64(UnixYear2048/(UnixYear2048-t.LastModified.Unix()))
+	// }
+	// shorter -> good
+
+	// larger -> good
+	var size int64 = 1
+	var timeTaken float64
+	if t.Size > 0 {
+		size = t.Size
+		timeTaken = 10 * (float64(time.Hour.Nanoseconds()) / float64(t.TimeTaken.Nanoseconds()))
+	}
+	// deeper -> good
+	deeper := t.Depth
+
+	return int64(float64(size*int64(deeper)) * (float64(1) / float64(timeTaken))) // * int64((float64(lastVisited) + float64(lastModified))
 }
