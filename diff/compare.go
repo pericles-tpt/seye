d38074634fbd42fb7a09a39219309aba79ba032c
pericles@outlook.com.au <pericles@outlook.com.au>
2023-05-14 21:19:19 +1000

    Move `compare.go`

--- diff/compare.go
+++ diff/compare.go
@@ -1,11 +1,16 @@
-package tree
+package diff
 
-import "time"
+import (
+	"bytes"
+	"time"
+
+	"github.com/Fiye/tree"
+)
 
 /*
-	Determine all the differences between two trees and store them in an output FileTree
+	Determine all the differences between two trees and store them in an output tree.FileTree
 */
-func CompareTrees(a, b *FileTree) FileTreeDiff {
+func CompareTrees(a, b *tree.FileTree) FileTreeDiff {
 	if a == nil && b == nil {
 		return FileTreeDiff{}
 	}
@@ -28,7 +33,7 @@ func CompareTrees(a, b *FileTree) FileTreeDiff {
 
 		for _, t := range b.SubTrees {
 			res := CompareTrees(nil, &t)
-			if !DiffEqual(res, FileTreeDiff{}) {
+			if !DiffEqual(res, FileTreeDiff{}, false) {
 				ret.SubTreesDiff = append(ret.SubTreesDiff, res)
 			}
 		}
@@ -38,7 +43,7 @@ func CompareTrees(a, b *FileTree) FileTreeDiff {
 	} else if b == nil {
 		ret := FileTreeDiff{
 			NewerPath:         a.BasePath,
-			FilesDiff:         []File{},
+			FilesDiff:         []tree.File{},
 			SubTreesDiff:      []FileTreeDiff{},
 			LastVisitedDiff:   time.Time{}.Sub(a.LastVisited),
 			TimeTakenDiff:     -a.TimeTaken,
@@ -53,7 +58,7 @@ func CompareTrees(a, b *FileTree) FileTreeDiff {
 
 		for _, t := range a.SubTrees {
 			res := CompareTrees(&t, nil)
-			if !DiffEqual(res, FileTreeDiff{}) {
+			if !DiffEqual(res, FileTreeDiff{}, false) {
 				ret.SubTreesDiff = append(ret.SubTreesDiff, res)
 			}
 		}
@@ -64,147 +69,15 @@ func CompareTrees(a, b *FileTree) FileTreeDiff {
 
 	retDiff := FileTreeDiff{}
 
-	// 1. Basic checks
-	// Ignored (highly variable)
-	//	- LastVisited
-	//  - TimeTaken
-	//	- Priority
-	simpleEqual := BasicTreesEqual(*a, *b)
-	if simpleEqual {
-		if a.BasePath == b.BasePath {
-			return retDiff
-		}
-		retDiff.NewerPath = b.BasePath
-		return retDiff
-	}
-
 	// 0. Need to determine how to compare files i.e. whether the trees are BOTH "Comprehensive"
 	retDiff.Comprehensive = (*a).Comprehensive && (*b).Comprehensive
 
-	// 2. Compare files at tree roots
-	differentFiles := []File{}
-	aFilesMap := map[string]*File{}
-	for _, f := range a.Files {
-		aFilesMap[f.Name] = &f
-	}
+	// TODO: Review file diff logic
+	differentFiles := getDiffFiles((*a).Files, (*b).Files, retDiff.Comprehensive)
 
-	// 2a. Find all EXACT matching files
-	notMatchedIndices := []int{}
-	for i, f := range b.Files {
-		aFile, ok := aFilesMap[f.Name]
-		if !ok || (ok && !FilesEqual(f, *aFile, retDiff.Comprehensive)) {
-			notMatchedIndices = append(notMatchedIndices, i)
-		} else {
-			aFilesMap[f.Name] = nil
-		}
-	}
-
-	// 2b. Now look for renamed, changed or removed files
-	for _, i := range notMatchedIndices {
-		var (
-			currFile          = b.Files[i]
-			exactMatchFoundAt = ""
-			sameNameFound     = (aFilesMap[currFile.Name] != nil)
-		)
-
-		for name, file := range aFilesMap {
-			if file == nil {
-				continue
-			}
-
-			if FilesEqual(b.Files[i], *file, retDiff.Comprehensive) {
-				exactMatchFoundAt = name
-				continue
-			}
-		}
-
-		if exactMatchFoundAt == "" && !sameNameFound { // -> added
-			differentFiles = append(differentFiles, currFile)
-		} else if exactMatchFoundAt != "" { // -> renamed
-			currFile.Name = exactMatchFoundAt
-			currFile.Size = 0
-			differentFiles = append(differentFiles, currFile)
-			aFilesMap[exactMatchFoundAt] = nil
-		} else if sameNameFound { // -> changed
-			currFile.Size -= aFilesMap[currFile.Name].Size
-			differentFiles = append(differentFiles, currFile)
-			aFilesMap[exactMatchFoundAt] = nil
-		}
-	}
-
-	// 2c. Anything left in aFilesMap has been REMOVED
-	for _, file := range aFilesMap {
-		if file != nil {
-			file.Size = -file.Size
-			differentFiles = append(differentFiles, *file)
-		}
-	}
-
+	// TODO: 3b and 3c below are probably wrong, causing large size of diff...
 	// 3. Compare subtrees
-	differentTrees := []FileTreeDiff{}
-	aFileTreesMap := map[string]*FileTree{}
-	for _, f := range a.SubTrees {
-		aFileTreesMap[f.BasePath] = &f
-	}
-
-	// 3a. Find all EXACT matching trees
-	notMatchedTreeIndices := []int{}
-	for i, f := range b.SubTrees {
-		aTree, ok := aFileTreesMap[f.BasePath]
-		if !ok || (ok && !BasicTreesEqual(f, *aTree)) {
-			notMatchedTreeIndices = append(notMatchedTreeIndices, i)
-		} else {
-			aFileTreesMap[f.BasePath] = nil
-		}
-	}
-
-	// 3b. Now look for renamed, changed or removed trees
-	for _, i := range notMatchedTreeIndices {
-		var (
-			currTree          = b.SubTrees[i]
-			exactMatchFoundAt = ""
-			samePathFound     = (aFileTreesMap[currTree.BasePath] != nil)
-		)
-
-		for path, tree := range aFileTreesMap {
-			if tree == nil {
-				continue
-			}
-
-			if BasicTreesEqual(b.SubTrees[i], *tree) {
-				exactMatchFoundAt = path
-				continue
-			}
-		}
-
-		if exactMatchFoundAt == "" && !samePathFound { // -> added
-			res := CompareTrees(nil, &currTree)
-			if !DiffEqual(res, FileTreeDiff{}) {
-				differentTrees = append(differentTrees, res)
-			}
-		} else if exactMatchFoundAt != "" { // -> renamed
-			res := CompareTrees(aFileTreesMap[exactMatchFoundAt], &currTree)
-			if !DiffEqual(res, FileTreeDiff{}) {
-				differentTrees = append(differentTrees, res)
-			}
-			aFileTreesMap[exactMatchFoundAt] = nil
-		} else if samePathFound { // -> changed
-			res := CompareTrees(aFileTreesMap[currTree.BasePath], &currTree)
-			if !DiffEqual(res, FileTreeDiff{}) {
-				differentTrees = append(differentTrees, res)
-			}
-			aFileTreesMap[exactMatchFoundAt] = nil
-		}
-	}
-
-	// 3c. Anything left in aFileTreesMap has been REMOVED
-	for _, tree := range aFileTreesMap {
-		res := CompareTrees(tree, nil)
-		if !DiffEqual(res, FileTreeDiff{}) {
-			differentTrees = append(differentTrees, res)
-		}
-
-	}
+	differentTrees := getDiffTrees((*a).SubTrees, (*b).SubTrees, retDiff.Comprehensive)
 
 	retDiff.NewerPath = b.BasePath
 	retDiff.FilesDiff = differentFiles
@@ -229,58 +102,30 @@ func CompareTrees(a, b *FileTree) FileTreeDiff {
 	return retDiff
 }
 
-func BasicTreesEqual(a, b FileTree) bool {
+func TreesEqual(a, b tree.FileTree, isComprehensive bool) bool {
+	if isComprehensive {
+		return DiffEqual(CompareTrees(&a, &b), FileTreeDiff{}, false)
+	}
 	return a.Depth == b.Depth &&
 		len(a.Files) == len(b.Files) &&
 		a.LastModified == b.LastModified &&
 		a.Size == b.Size &&
-		len(a.SubTrees) == len(b.SubTrees)
+		a.NumFilesTotal == b.NumFilesTotal &&
+		len(a.SubTrees) == len(b.SubTrees) &&
+		a.Comprehensive == b.Comprehensive
 }
 
-func FilesEqual(a, b File, isComprehensive bool) bool {
+func FilesEqual(a, b tree.File, isComprehensive bool) bool {
 	if isComprehensive {
-		return a.Size == b.Size && a.ByteSample == b.ByteSample
-	} else {
-		return a.Size == b.Size
+		return a.Size == b.Size && hashesOrRawEqual(a.Hash, b.Hash)
 	}
-}
-
-/*
-	Checks everything except for basepath
-*/
-func TreesEqual(a, b FileTree) bool {
-	isEqual := a.Depth == b.Depth
-	isEqual = isEqual && (a.LastModified == b.LastModified)
-	// Ignore `Priority`, `LastVisited` and `TimeTaken`, very sensitive
-	isEqual = isEqual && (a.Size == b.Size)
-	isEqual = isEqual && filesEqual(a.Files, b.Files)
-
-	isEqual = len(a.SubTrees) == len(b.SubTrees)
-	if !isEqual {
-		return isEqual
-	}
-	for i, t := range a.SubTrees {
-		isEqual = isEqual && TreesEqual(t, b.SubTrees[i])
-	}
-	return isEqual
-}
-
-func filesEqual(a, b []File) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	isEqual := true
-	for i, f := range a {
-		isEqual = isEqual && (f == b[i])
-	}
-	return isEqual
+	return a.Size == b.Size
 }
 
 /*
 	NOTE: Not very thorough
 */
-func DiffEqual(a, b FileTreeDiff) bool {
+func DiffEqual(a, b FileTreeDiff, isComprehensive bool) bool {
 	return len(a.FilesDiff) == len(b.FilesDiff) &&
 		a.LastModifiedDiff == b.LastModifiedDiff &&
 		a.LastVisitedDiff == b.LastVisitedDiff &&
@@ -288,4 +133,140 @@ func DiffEqual(a, b FileTreeDiff) bool {
 		a.SizeDiff == b.SizeDiff &&
 		len(a.SubTreesDiff) == len(b.SubTreesDiff) &&
 		a.TimeTakenDiff == b.TimeTakenDiff
+}
+
+func hashesOrRawEqual(a, b *tree.Hash) bool {
+	if a.Type != b.Type {
+		return false
+	}
+	return (a == nil && b == nil) || a != nil && b != nil && bytes.Equal((a).Bytes[:], (b).Bytes[:])
+}
+
+func getDiffFiles(a, b []tree.File, isComprehensive bool) []tree.File {
+	differentFiles := []tree.File{}
+
+	aFilesMap := map[string]*tree.File{}
+	for _, f := range a {
+		aFilesMap[f.Name] = &f
+	}
+
+	// 2a. Find all EXACT matching files
+	notMatchedIndices := []int{}
+	for i, f := range b {
+		aFile, ok := aFilesMap[f.Name]
+		if !ok || !FilesEqual(f, *aFile, isComprehensive) {
+			notMatchedIndices = append(notMatchedIndices, i)
+		} else {
+			aFilesMap[f.Name] = nil
+		}
+	}
+
+	// 2b. Now look for renamed, changed or removed files
+	for _, i := range notMatchedIndices {
+		var (
+			currFile          = b[i]
+			exactMatchFoundAt = ""
+			sameNameFound     = (aFilesMap[currFile.Name] != nil)
+		)
+
+		for name, file := range aFilesMap {
+			if file == nil {
+				continue
+			}
+
+			if FilesEqual(b[i], *file, isComprehensive) {
+				exactMatchFoundAt = name
+				continue
+			}
+		}
+
+		if exactMatchFoundAt == "" && !sameNameFound { // -> added
+			differentFiles = append(differentFiles, currFile)
+		} else if exactMatchFoundAt != "" { // -> renamed
+			currFile.Name = exactMatchFoundAt
+			currFile.Size = 0
+			differentFiles = append(differentFiles, currFile)
+			aFilesMap[exactMatchFoundAt] = nil
+		} else if sameNameFound { // -> changed
+			currFile.Size -= aFilesMap[currFile.Name].Size
+			differentFiles = append(differentFiles, currFile)
+			aFilesMap[exactMatchFoundAt] = nil
+		}
+	}
+
+	// 2c. Anything left in aFilesMap has been REMOVED
+	for _, file := range aFilesMap {
+		if file != nil {
+			file.Size = -file.Size
+
+			differentFiles = append(differentFiles, *file)
+		}
+	}
+
+	return differentFiles
+}
+
+func getDiffTrees(a, b []tree.FileTree, isComprehensive bool) []FileTreeDiff {
+	moreTrees := len(a)
+	if len(b) > len(a) {
+		moreTrees = len(b)
+	}
+	differentTrees := make([]FileTreeDiff, moreTrees)
+	aFileTreesMap := map[string]*tree.FileTree{}
+	for _, f := range a {
+		aFileTreesMap[f.BasePath] = &f
+	}
+
+	// 3a. Find all EXACT matching trees
+	notMatchedTreeIndices := []int{}
+	for i, f := range b {
+		aTree, ok := aFileTreesMap[f.BasePath]
+		if !ok || (ok && !TreesEqual(f, *aTree, isComprehensive)) {
+			notMatchedTreeIndices = append(notMatchedTreeIndices, i)
+		} else {
+			aFileTreesMap[f.BasePath] = nil
+			differentTrees[i] = FileTreeDiff{}
+		}
+	}
+
+	// 3b. Now look for renamed, changed or removed trees
+	for _, i := range notMatchedTreeIndices {
+		var (
+			currTree          = b[i]
+			exactMatchFoundAt = ""
+			samePathFound     = (aFileTreesMap[currTree.BasePath] != nil)
+		)
+
+		for path, tree := range aFileTreesMap {
+			if tree == nil {
+				continue
+			}
+
+			if TreesEqual(b[i], *tree, isComprehensive) {
+				exactMatchFoundAt = path
+				break
+			}
+		}
+
+		if exactMatchFoundAt == "" && !samePathFound { // -> added
+			res := CompareTrees(nil, &currTree)
+			differentTrees[i] = res
+		} else if exactMatchFoundAt != "" { // -> renamed
+			res := CompareTrees(aFileTreesMap[exactMatchFoundAt], &currTree)
+			differentTrees[i] = res
+			aFileTreesMap[exactMatchFoundAt] = nil
+		} else if samePathFound { // -> changed
+			res := CompareTrees(aFileTreesMap[currTree.BasePath], &currTree)
+			differentTrees[i] = res
+			aFileTreesMap[exactMatchFoundAt] = nil
+		}
+	}
+
+	// 3c. Anything left in aFileTreesMap has been REMOVED
+	for _, tree := range aFileTreesMap {
+		res := CompareTrees(tree, nil)
+		differentTrees = append(differentTrees, res)
+	}
+
+	return differentTrees
 }
