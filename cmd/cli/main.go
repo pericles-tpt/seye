bf92a3fc97804b0453e3a3d72b51f3b427469c11
pericles@outlook.com.au <pericles@outlook.com.au>
2023-05-18 22:28:18 +1000

    Add testing commands "diffTest" and "readWriteBenchmark" to cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math"
+	"os"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/Fiye/diff"
 	"github.com/Fiye/tree"
+	u "github.com/bcicen/go-units"
 	"github.com/boynton/repl"
+	"github.com/davecgh/go-spew/spew"
 )
 
 type TestHandler struct {
@@ -46,6 +51,13 @@ func (th *TestHandler) Eval(expr string) (string, bool, error) {
 
 		output := runScan(args[1], numFiles)
 		return output, false, nil
+	case "diffTest":
+		spew.Dump([32]byte{})
+		runDiffTest("/home/pt")
+		return "", false, nil
+	case "readWriteBenchmark":
+		runBinaryReadWriteTest()
+		return "", false, nil
 	case "schedule":
 		return "unimplemented", true, nil
 	case "view":
@@ -102,8 +114,111 @@ func runScan(path string, numLargestFiles int64) string {
 			break
 		}
 
-		output += fmt.Sprintf("%s %db\n", f.FullName, f.Size)
+		val := u.NewValue(float64(f.Size), u.Byte)
+		output += fmt.Sprintf("%s %fgb\n", f.FullName, val.MustConvert(u.GigaByte).Float())
 	}
 
 	return output + fmt.Sprintf("Took: %dms", time.Since(timer).Milliseconds())
+}
+
+func runDiffTest(path string) {
+	fmt.Println("Started walk 1")
+	s1, _, _, _, _ := tree.Walk(path, 0, true, nil)
+	fmt.Printf("Finished walk 1, num files: %d\n", s1.NumFilesTotal)
+
+	fmt.Println("Sleeping for 1 minute...")
+	time.Sleep(1 * time.Minute)
+
+	fmt.Println("Started walk 2")
+	s2, _, _, _, _ := tree.Walk(path, 0, true, nil)
+	fmt.Printf("Finished walk 2, num files: %d\n", s2.NumFilesTotal)
+
+	fmt.Println("Started diff")
+	d := diff.CompareTrees(&s1, &s2)
+	fmt.Printf("Finished diff, diff num files: %d\n", d.NumFilesTotalDiff)
+
+	s1_s2 := diff.WalkAddDiff(s1, d)
+	fmt.Printf("The result of s1 + diff(s1, s2) == s2 is: %v\n", diff.TreeDiffEmpty(diff.CompareTrees(&s1_s2, &s2)))
+	fmt.Printf("Num files in the added diff is: %d\n", s1_s2.NumFilesTotal)
+}
+
+func runBinaryReadWriteTest() {
+	os.Remove("./s1.gob")
+
+	path := "/home/pt"
+	fmt.Printf("BENCHMARK PERFORMED ON PATH '%s'\n\n", path)
+
+	fmt.Println("READ/WRITE TEST OF 'SHALLOW' SCAN (NO SHA256 HASHES)")
+	fmt.Println("started creating big tree")
+	s1, _, _, _, _ := tree.Walk(path, 0, false, nil)
+	fmt.Printf("walk took: %dms\n\n", s1.TimeTaken.Milliseconds())
+
+	var fileSize int64 = 0
+
+	fmt.Println("doing test write to get size")
+	err := tree.WriteBinary(s1, "./s1.gob")
+	if err != nil {
+		panic(err)
+	}
+	f, _ := os.Stat("./s1.gob")
+	fileSize = f.Size()
+	os.Remove("./s1.gob")
+	units, _ := u.ConvertFloat(float64(fileSize), u.Byte, u.MegaByte)
+	fmt.Printf("tree is of size %.2f %s\n\n", units.Float(), units.Unit().PluralName())
+
+	fmt.Println("started writing big tree (actual)")
+	timer := time.Now()
+	err = tree.WriteBinary(s1, "./s1.gob")
+	if err != nil {
+		panic(err)
+	}
+	endTimer := time.Since(timer)
+	fmt.Printf("write took: %dms, at a speed of %.2f %s/s\n\n", endTimer.Milliseconds(), units.Float()/(float64(endTimer.Nanoseconds())/math.Pow(10, 9)), units.Unit().Name)
+
+	fmt.Println("started reading big tree into memory")
+	timer = time.Now()
+	s2, err := tree.ReadBinary("./s1.gob")
+	if err != nil {
+		panic(err)
+	}
+	endTimer = time.Since(timer)
+	fmt.Printf("read took: %dms, at a speed of %.2f %s/s\n", endTimer.Milliseconds(), (units.Float() / (float64(endTimer.Nanoseconds()) / math.Pow(10, 9))), units.Unit().Name)
+	s2.Depth += 1
+	os.Remove("./s1.gob")
+
+	fmt.Println("\nREAD/WRITE TEST OF 'FULL' SCAN (INCLUDES SHA256 HASHES)")
+	fmt.Println("started creating big tree")
+	s1, _, _, _, _ = tree.Walk(path, 0, true, nil)
+	fmt.Printf("walk took: %dms\n\n", s1.TimeTaken.Milliseconds())
+
+	fmt.Println("doing test write to get size")
+	err = tree.WriteBinary(s1, "./s1.gob")
+	if err != nil {
+		panic(err)
+	}
+	f, _ = os.Stat("./s1.gob")
+	fileSize = f.Size()
+	os.Remove("./s1.gob")
+	units, _ = u.ConvertFloat(float64(fileSize), u.Byte, u.MegaByte)
+	fmt.Printf("tree is of size %.2f %s\n\n", units.Float(), units.Unit().PluralName())
+
+	fmt.Println("started writing big tree (actual)")
+	timer = time.Now()
+	err = tree.WriteBinary(s1, "./s1.gob")
+	if err != nil {
+		panic(err)
+	}
+	endTimer = time.Since(timer)
+	fmt.Printf("write took: %dms, at a speed of %.2f %s/s\n\n", endTimer.Milliseconds(), units.Float()/(float64(endTimer.Nanoseconds())/math.Pow(10, 9)), units.Unit().Name)
+
+	fmt.Println("started reading big tree into memory")
+	timer = time.Now()
+	s2, err = tree.ReadBinary("./s1.gob")
+	if err != nil {
+		panic(err)
+	}
+	endTimer = time.Since(timer)
+	fmt.Printf("read took: %dms, at a speed of %.2f %s/s\n", endTimer.Milliseconds(), (units.Float() / (float64(endTimer.Nanoseconds()) / math.Pow(10, 9))), units.Unit().Name)
+	s2.Depth += 1
+	os.Remove("./s1.gob")
 }
