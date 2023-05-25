23855e146439c0bc7a0c81730004ce99254ed86a
pericles@outlook.com.au <pericles@outlook.com.au>
2023-05-25 21:17:57 +1000

    Change `WriteBinary` to method, add `DeepCopy` function to do DeepCopy of `FileTree`s

--- tree/file.go
+++ tree/file.go
@@ -1,15 +1,14 @@
 package tree
 
 import (
+	"bytes"
 	"encoding/gob"
-	"encoding/json"
 	"os"
 
 	"github.com/joomcode/errorx"
 )
 
-// TODO: Should revisit these, JSON (although easier to read) isn't as efficient as a binary file format (review `gob` library for recursive data structures)
-func WriteBinary(tree any, path string) error {
+func (tree *FileTree) WriteBinary(path string) error {
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		return errorx.Decorate(err, "failed to opem/create file for writing FileTree data")
@@ -30,31 +29,19 @@ func ReadBinary(path string) (FileTree, error) {
 
 	gd := gob.NewDecoder(f)
 	err = gd.Decode(&tree)
-
 	return tree, err
 }
 
-func WriteJSON(tree any, path string) error {
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		return errorx.Decorate(err, "failed to opem/create file for writing FileTree data")
-	}
-	defer f.Close()
+func (tree *FileTree) DeepCopy() FileTree {
+	var (
+		b       bytes.Buffer
+		newTree FileTree
+	)
+	ge := gob.NewEncoder(&b)
+	ge.Encode(tree)
 
-	je := json.NewEncoder(f)
-	return je.Encode(tree)
-}
+	gd := gob.NewDecoder(&b)
+	gd.Decode(&newTree)
 
-func ReadJSON(path string) (FileTree, error) {
-	tree := FileTree{}
-	f, err := os.OpenFile(path, os.O_RDONLY, 0400)
-	if err != nil {
-		return tree, errorx.Decorate(err, "failed to open file for readgin FileTree data")
-	}
-	defer f.Close()
-
-	jd := json.NewDecoder(f)
-	err = jd.Decode(tree)
-
-	return tree, err
+	return newTree
 }
