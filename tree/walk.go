dd670506d7b7b0ace26d43417db2481966a6cad8
pericles@outlook.com.au <pericles@outlook.com.au>
2023-05-25 21:18:43 +1000

    Simplify `WalkGenerateTree` logic

--- tree/walk.go
+++ tree/walk.go
@@ -1,28 +1,39 @@
 package tree
 
 import (
-	"bytes"
 	"crypto/sha256"
 	"fmt"
-	"io/fs"
+	"io"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/Fiye/file"
+	"github.com/Fiye/stats"
+	"github.com/Fiye/utility"
 	"github.com/joomcode/errorx"
 )
 
 var (
-	Here              = time.Local
-	UnixYear2048      = time.Date(2025, 1, 1, 0, 0, 0, 0, Here).Unix()
-	AllTrees          = []FileTree{}
-	ignoredDirs       = []string{"proc", "Fiye"}
-	chosenHash        = SHA256
-	numFiles          = 0
-	largestFilesLimit = 100
+	Here         = time.Local
+	UnixYear2048 = time.Date(2025, 1, 1, 0, 0, 0, 0, Here).Unix()
+	AllTrees     = []FileTree{}
+	ignoredDirs  = []string{"proc", "Fiye"}
+	chosenHash   = sha256.Size
+	copyBuffer   = make([]byte, 1048576)
+
+	filesAddedForHashing = 0
+	noHashOffset         = -1
+
+	AllHashBytes = []byte{}
 )
 
-func Walk(path string, depth int, isComprehensive bool, largestFiles *[]LargeFile) (tree FileTree, errsBelow []string, size int64, numFilesR int64, latestModified *time.Time) {
+func WalkGenerateTree(path string, depth int, isComprehensive bool, walkStats *stats.WalkStats) (tree FileTree) {
+	if depth == 0 {
+		filesAddedForHashing = 0
+		AllHashBytes = []byte{}
+	}
+
 	tree = FileTree{
 		BasePath: path,
 	}
@@ -35,9 +46,10 @@ func Walk(path string, depth int, isComprehensive bool, largestFiles *[]LargeFil
 	}
 
 	var (
-		newestModified     *time.Time = nil
-		startWalk                     = time.Now()
-		numFilesInSubtrees int64      = 0
+		// TODO: Setting newestModified to `time.Time{}` might cause issues if a file is incidentally created at the time...
+		newestModified           = time.Time{}
+		startWalk                = time.Now()
+		numFilesInSubtrees int64 = 0
 	)
 	for _, e := range ents {
 		fullPath := fmt.Sprintf("%s/%s", tree.BasePath, e.Name())
@@ -46,63 +58,36 @@ func Walk(path string, depth int, isComprehensive bool, largestFiles *[]LargeFil
 		}
 
 		if e.IsDir() {
-			if contains(ignoredDirs, e.Name()) {
+			if utility.Contains(ignoredDirs, e.Name()) {
 				continue
 			}
 
-			subTree, err, subSize, subNumFiles, currModified := Walk(fullPath, depth+1, isComprehensive, largestFiles)
-			if len(err) > 0 {
-				tree.ErrStrings = append(tree.ErrStrings, err...)
+			subTree := WalkGenerateTree(fullPath, depth+1, isComprehensive, walkStats)
+			if len(subTree.ErrStrings) > 0 {
+				tree.ErrStrings = append(tree.ErrStrings, subTree.ErrStrings...)
 			}
-			tree.Size += subSize
-			numFilesInSubtrees += subNumFiles
+			tree.Size += subTree.Size
+			numFilesInSubtrees += subTree.NumFilesTotal
 
 			tree.SubTrees = append(tree.SubTrees, subTree)
-			newestModified = GetNewestTime(newestModified, currModified)
+			newestModified = utility.GetNewestTime(newestModified, subTree.LastModified)
 		} else if e.Type().IsRegular() {
 			fStat, err := e.Info()
-			nf := File{
-				Err:  "",
-				Hash: nil,
+			nf := file.File{
+				Hash: file.HashLocation{
+					HashOffset: &noHashOffset,
+				},
 			}
 			if err != nil {
 				tree.ErrStrings = append(tree.ErrStrings, errorx.Decorate(err, "failed to stat file").Error())
 			} else {
-				if isComprehensive {
-					nf.Hash = &Hash{}
+				nf = getFileData(fullPath, fStat, isComprehensive, &tree, walkStats)
 
-					fTmp, err := os.OpenFile(fullPath, os.O_RDONLY, 0400)
-					if err != nil {
-						tree.ErrStrings = append(tree.ErrStrings, errorx.Decorate(err, "failed to open file to get `Hash` for 'Comprehensive' scan").Error())
-					} else if fStat.Size() > 0 {
-						var b bytes.Buffer
-
-						n, err := b.ReadFrom(fTmp)
-						if err != nil {
-							tree.ErrStrings = append(tree.ErrStrings, errorx.Decorate(err, "failed to read WHOLE file").Error())
-						}
-
-						if n <= int64(chosenHash) {
-							nf.Hash.Type = NONE
-							copy(nf.Hash.Bytes[:], b.Bytes())
-						} else {
-							nf.Hash.Type = chosenHash
-							nf.Hash.Bytes = generateSHAHash(b.Bytes())
-						}
-					}
+				if walkStats != nil {
+					walkStats.UpdateLargestFiles(fullPath, fStat)
 				}
 
-				if largestFiles != nil {
-					updateLargestFiles(largestFiles, fullPath, fStat)
-				}
-
-				nf.Name = fStat.Name()
-				nf.Size = fStat.Size()
-				nf.LastModified = fStat.ModTime()
-
-				tree.Size += fStat.Size()
-				currModified := fStat.ModTime()
-				newestModified = GetNewestTime(newestModified, &currModified)
+				newestModified = utility.GetNewestTime(newestModified, nf.LastModified)
 			}
 
 			tree.Files = append(tree.Files, nf)
@@ -114,119 +99,56 @@ func Walk(path string, depth int, isComprehensive bool, largestFiles *[]LargeFil
 	tree.LastVisited = time.Now()
 	tree.Depth = depth
 
-	return tree, tree.ErrStrings, tree.Size, tree.NumFilesTotal, newestModified
+	if tree.Depth == 0 && len(AllHashBytes) > 0 {
+		tree.AllHash = AllHashBytes
+	}
+
+	return tree
 }
 
-func GetNewestTime(curr *time.Time, new *time.Time) *time.Time {
-	if curr == nil || (new != nil && curr.Before(*new)) {
-		return new
-	}
-	return curr
-}
-
-// TODO: Delete this?
-func PrintTree(t *FileTree) {
-	if t == nil {
-		return
-	}
-	fmt.Printf("On tree with path: %s, size at this level is: %d\n", t.BasePath, t.Size)
-
-	for _, f := range t.Files {
-		fmt.Printf("	Has file %s of size: %d\n", f.Name, f.Size)
+func getFileData(fullPath string, fStat os.FileInfo, isComprehensive bool, tree *FileTree, walkStats *stats.WalkStats) file.File {
+	nf := file.File{
+		Hash: file.HashLocation{
+			HashOffset: &noHashOffset,
+		},
 	}
 
-	for _, t := range t.SubTrees {
-		PrintTree(&t)
-	}
-}
-
-func contains(arr []string, target string) bool {
-	for _, v := range arr {
-		if v == target {
-			return true
-		}
-	}
-	return false
-}
-
-func generateSHAHash(bs []byte) [sha256.Size]byte {
-	return sha256.Sum256(bs)
-}
-
-func updateLargestFiles(largestFiles *[]LargeFile, filePath string, fInfo fs.FileInfo) {
-	// Start from the bottom find the first place to insert it (if we can)
-	var (
-		thisSize         = fInfo.Size()
-		smallerFileIndex = -1
-		largerFileIndex  = -1
-	)
-
-	if len((*largestFiles)) == 0 {
-		*largestFiles = append(*largestFiles, LargeFile{FullName: filePath, Size: fInfo.Size()})
-		return
-	}
-
-	for i, _ := range *largestFiles {
-		currIndex := len(*largestFiles) - 1 - i
-		if currIndex < 0 {
-			break
-		}
-		currFile := (*largestFiles)[currIndex]
-
-		if thisSize > currFile.Size {
-			smallerFileIndex = currIndex
-		} else if thisSize <= currFile.Size {
-			largerFileIndex = currIndex
-		}
-
-		if largerFileIndex > -1 {
-			if smallerFileIndex == -1 {
-				if len((*largestFiles)) < largestFilesLimit {
-					// Append to the end (new smallest file)
-					newLargeFile := LargeFile{
-						FullName: filePath,
-						Size:     fInfo.Size(),
-					}
-
-					(*largestFiles) = append((*largestFiles), newLargeFile)
-				}
-				break
+	if isComprehensive {
+		fTmp, err := os.OpenFile(fullPath, os.O_RDONLY, 0400)
+		if err != nil {
+			(*tree).ErrStrings = append((*tree).ErrStrings, errorx.Decorate(err, "failed to open file to get `Hash` for 'Comprehensive' scan").Error())
+		} else if fStat.Size() > 0 {
+			h := sha256.New()
+			_, err := io.CopyBuffer(h, fTmp, copyBuffer)
+			if err != nil {
+				(*tree).ErrStrings = append((*tree).ErrStrings, errorx.Decorate(err, "failed to read WHOLE file").Error())
 			} else {
-				// Insert between two items
-				// shiftDownAfter(largestFiles, smallerFileIndex)
+				newHashOffset := len(AllHashBytes)
+				nf.Hash.Type = file.SHA256
+				nf.Hash.HashOffset = &newHashOffset
+				hashedBytes := h.Sum(nil)
+				// TODO: Change hardcoded `treeThreads` index once we implement multithreading
+				AllHashBytes = append(AllHashBytes, hashedBytes[:]...)
 
-				(*largestFiles)[smallerFileIndex] = LargeFile{
-					FullName: filePath,
-					Size:     fInfo.Size(),
+				if walkStats != nil {
+					walkStats.UpdateDuplicates(hashedBytes[:], fullPath)
 				}
 
-				break
-			}
-		} else if smallerFileIndex > -1 && currIndex == 0 {
-			// Prepend to the start
-			newLargestFiles := []LargeFile{
-				{
-					FullName: filePath,
-					Size:     fInfo.Size(),
-				},
+				nf.Hash.HashLength = len(hashedBytes)
 			}
 
-			(*largestFiles) = append(newLargestFiles, (*largestFiles)...)
-
-			if len((*largestFiles)) > largestFilesLimit {
-				*largestFiles = (*largestFiles)[:largestFilesLimit-1]
-			}
-			break
 		}
 	}
-}
 
-// TODO: This is broken, should be using it...
-// func shiftDownAfter(largestFiles *[]LargeFile, shiftDownAfterIndex int) {
-// 	for i, _ := range *largestFiles {
-// 		if i >= (shiftDownAfterIndex) {
-// 			tmp := (*largestFiles)[i]
-// 			(*largestFiles)[i+1] = tmp
-// 		}
-// 	}
-// }
+	// 260/490 (ish) - 64000
+	// 297/524 - 128000
+	//
+
+	nf.Name = fullPath
+	nf.Size = fStat.Size()
+	nf.LastModified = fStat.ModTime()
+
+	(*tree).Size += fStat.Size()
+
+	return nf
+}
