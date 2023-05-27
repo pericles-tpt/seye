4854fe7c0fd83a479979390e04fb82593d0035b2
Pericles Telemachou <pericles@outlook.com.au>
2023-05-27 23:25:32 +1000

    Change `ReadBinary` and `WriteBinary` in `diff` to act on `ScanDiff`

--- diff/file.go
+++ diff/file.go
@@ -7,10 +7,10 @@ import (
 	"github.com/joomcode/errorx"
 )
 
-func (d *TreeDiff) WriteBinary(path string) error {
+func (d *ScanDiff) WriteBinary(path string) error {
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
-		return errorx.Decorate(err, "failed to opem/create file for writing FileTree data")
+		return errorx.Decorate(err, "failed to open/create file for writing ScanDiff data")
 	}
 	defer f.Close()
 
@@ -18,27 +18,16 @@ func (d *TreeDiff) WriteBinary(path string) error {
 	return ge.Encode(&d)
 }
 
-func (d *DiffMaps) WriteBinary(path string) error {
-	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		return errorx.Decorate(err, "failed to opem/create file for writing FileTree data")
-	}
-	defer f.Close()
-
-	ge := gob.NewEncoder(f)
-	return ge.Encode(&d)
-}
-
-func ReadBinary(path string) (TreeDiff, error) {
-	treeDiff := TreeDiff{}
+func ReadBinary(path string) (ScanDiff, error) {
+	scanDiff := ScanDiff{}
 	f, err := os.OpenFile(path, os.O_RDONLY, 0400)
 	if err != nil {
-		return treeDiff, errorx.Decorate(err, "failed to open file for readgin FileTree data")
+		return scanDiff, errorx.Decorate(err, "failed to open file for readgin FileTree data")
 	}
 	defer f.Close()
 
 	gd := gob.NewDecoder(f)
-	err = gd.Decode(&treeDiff)
+	err = gd.Decode(&scanDiff)
 
-	return treeDiff, err
+	return scanDiff, err
 }
