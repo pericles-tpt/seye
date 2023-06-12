0065ee36e7582bacab2cd60df109e81e55c75bf0
Pericles Telemachou <pericles@outlook.com.au>
2023-06-12 10:37:06 +1000

    Add labelling for type of file change to `diff` command

--- diff/print.go
+++ diff/print.go
@@ -3,6 +3,17 @@ package diff
 import (
 	"fmt"
 	"sort"
+	"strings"
+)
+
+var (
+	diffTypeToString = map[DiffType]string{
+		modified: "modified",
+		same:     "same",
+		renamed:  "renamed",
+		removed:  "removed",
+		added:    "added",
+	}
 )
 
 func PrintLargestDiffs(limit int, sf ScanDiff) {
@@ -31,14 +42,14 @@ func PrintLargestDiffs(limit int, sf ScanDiff) {
 	fmt.Println("Biggest disk usage INCREASES")
 	for i := 0; i < limit && i < len(deepestDirs); i++ {
 		if deepestDirs[i].SizeDiff > 0 {
-			fmt.Printf("'%s' +%d bytes\n", deepestDirs[i].NewerName, deepestDirs[i].SizeDiff)
+			fmt.Printf("'%s' +%d bytes (%s)\n", deepestDirs[i].NewerName, deepestDirs[i].SizeDiff, strings.ToUpper(diffTypeToString[deepestDirs[i].Type]))
 		}
 	}
 
 	fmt.Println("\nBiggest disk usage DECREASES")
 	for i := 0; i < limit && i < len(deepestDirs); i++ {
-		if deepestDirs[len(deepestDirs)-1-i].SizeDiff < 0 {
-			fmt.Printf("'%s' %d bytes\n", deepestDirs[len(deepestDirs)-1-i].NewerName, deepestDirs[len(deepestDirs)-1-i].SizeDiff)
+		if deepestDirs[len(deepestDirs)-1-i].SizeDiff <= 0 {
+			fmt.Printf("'%s' %d bytes (%s)\n", deepestDirs[len(deepestDirs)-1-i].NewerName, deepestDirs[len(deepestDirs)-1-i].SizeDiff, strings.ToUpper(diffTypeToString[deepestDirs[len(deepestDirs)-1-i].Type]))
 		}
 	}
 }
